af4df336d3b97f07123b4764570f0fb1c446c621
Andrey Babincev <andron-2102@mail.ru>
2023-05-19 18:36:55 +0400

    added * simple inMemoryAuth

--- src/main/java/ru/babincev/trainingweb/config/MySpringConfig.java
+++ src/main/java/ru/babincev/trainingweb/config/MySpringConfig.java
@@ -1,9 +1,40 @@
 package ru.babincev.trainingweb.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class MySpringConfig {
+@EnableWebSecurity
+public class MySpringConfig extends WebSecurityConfigurerAdapter{
+
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeRequests().antMatchers("/trainers/{\\d+}/edit").authenticated()
+                .and().authorizeRequests().antMatchers("/trainers/{\\d+}/add-appointment").authenticated()
+                .and().authorizeRequests().antMatchers("/trainers/new").authenticated()
+                .and().authorizeRequests().anyRequest().permitAll()
+                .and().httpBasic();
+
+    }
+    @Autowired
+    @Lazy
+    public void configureGlobal(AuthenticationManagerBuilder authentication)
+            throws Exception
+    {
+        authentication.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("USER");
+    }
 
 }
